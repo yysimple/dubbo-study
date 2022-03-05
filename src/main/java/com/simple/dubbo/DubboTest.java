e392b6274f0eab4be8a07f0432f8aa17eadcacb4
wuchengxing <1449697757@qq.com>
2022-03-05 17:43:17 +0800

    feat：初始化dubbo项目

---
+++ src/main/java/com/simple/dubbo/DubboTest.java
@@ -0,0 +1,16 @@
+package com.simple.dubbo;
+
+/**
+ * 项目: dubbo-study
+ * <p>
+ * 功能描述: dubbo学习案例
+ *
+ * @author: WuChengXing
+ * @create: 2022-03-05 13:13
+ **/
+public class DubboTest {
+    public static void main(String[] args) {
+        System.out.println("dubbo测试，初始化环境");
+    }
+
+}
